3d67c8747897e9dae8612a0585bd040e5bf1bf19
Ibrahim <habib-islam.mansour-ibrahim@informatik.hs-fulda.de>
2026-04-04 00:25:06 +0200

    Validate, structure and store messages

--- index.js
+++ index.js
@@ -4,17 +4,31 @@ const port = 3000;
 
 app.use(express.json());
 
+const messages = [];
+
 app.get("/", (req, res) => {
-  res.send(`Server is running on port ${port}`);
+  res.send(`Server is running on port ${port}...`);
 });
 
 app.get("/messages", (req, res) => {
-  res.json([]);
+  res.json(messages);
 });
 
 app.post("/messages", (req, res) => {
-  const message = req.body;
-  res.status(201).json(message);
+  const { text, sender } = req.body;
+
+  if (!text || !sender) {
+    return res.status(400).json({ error: "Text and its sender are required!" });
+  }
+
+  const message = {
+    id: messages.length,
+    text: text,
+    sender: sender,
+    createdAt: new Date().toISOString()
+  }
+  messages.push(message);
+  res.status(201).send("Message sent.");
 })
 
 app.listen(port, () => {
