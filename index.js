7961af2fdc24e2f5f93fd007eef2abc1615c4489
Ibrahim <habib-islam.mansour-ibrahim@informatik.hs-fulda.de>
2026-04-17 20:41:09 +0200

    Add PostgreSQL connection using environment variables

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+const path = require("path");
+require("dotenv").config({
+  override: true,
+  path: path.join( __dirname, ".env")
+});
+
+const { Pool } = require("pg");
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT
+});
+
 const express = require("express");
 const app = express();
 const port = 3000;
